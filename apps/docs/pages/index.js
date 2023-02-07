314d559effc42a12baa49f047d52bfbbb9bda1cd
Usman <36886437+usafder@users.noreply.github.com>
2023-02-08 01:56:23 +0500

    update title in docs page

--- apps/docs/pages/index.js
+++ apps/docs/pages/index.js
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Docs() {
   return (
     <>
-      <h1>Docs</h1>
+      <h1>Docs app</h1>
       <p>docs app in a monorepo</p>
       <Button />
     </>
