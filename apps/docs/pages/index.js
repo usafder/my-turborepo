817a61f440aadf8026f783c71e644968f10a5cc2
Usman <36886437+usafder@users.noreply.github.com>
2023-02-08 01:09:09 +0500

    removed div from docs app page

--- apps/docs/pages/index.js
+++ apps/docs/pages/index.js
@@ -2,10 +2,10 @@ import { Button } from "ui";
 
 export default function Docs() {
   return (
-    <div>
+    <>
       <h1>Docs</h1>
       <p>docs app in a monorepo</p>
       <Button />
-    </div>
+    </>
   );
 }
