ef1b9bcfd1ce12687e64303f6d5510a9f1616c4f
Usman <36886437+usafder@users.noreply.github.com>
2023-02-08 01:13:54 +0500

    removed div from web app's page

--- apps/web/pages/index.js
+++ apps/web/pages/index.js
@@ -2,10 +2,10 @@ import { Button } from "ui";
 
 export default function Web() {
   return (
-    <div>
+    <>
       <h1>Web App</h1>
       <p>web app in a monorepo</p>
       <Button />
-    </div>
+    </>
   );
 }
