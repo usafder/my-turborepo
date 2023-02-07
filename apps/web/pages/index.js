5f893a6123f0b97f0ff5b7769b0fe4b5267076a7
Usman <36886437+usafder@users.noreply.github.com>
2023-02-08 01:25:21 +0500

    updated title in web page

--- apps/web/pages/index.js
+++ apps/web/pages/index.js
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <>
-      <h1>Web App</h1>
+      <h1>Web</h1>
       <p>web app in a monorepo</p>
       <Button />
     </>
